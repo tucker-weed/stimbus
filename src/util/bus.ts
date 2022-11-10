905bec52117fae431b9743b168f73bcaf3b25f31
Tucker Weed <tuckerjweed@gmail.com>
2022-11-10 11:39:09 -0500

    [chore] run lint

--- src/util/bus.ts
+++ src/util/bus.ts
@@ -1,30 +1,30 @@
 export default class EventBus {
-  #bus: Document
-  #events: Record<string, Partial<Record<EventKey, Listener>>>
+  #bus: Document;
+  #events: Record<string, Partial<Record<EventKey, Listener>>>;
 
   constructor() {
-    this.#bus = document
-    this.#events = {}
+    this.#bus = document;
+    this.#events = {};
   }
 
   addEvent(callerId: string, type: EventKey, listener: Listener) {
-    this.#bus.addEventListener(type, listener)
+    this.#bus.addEventListener(type, listener);
     if (!this.#events[callerId]) {
-      this.#events[callerId] = {}
+      this.#events[callerId] = {};
     }
-    this.#events[callerId][type] = listener
+    this.#events[callerId][type] = listener;
   }
 
   removeEvent(callerId: string, type: EventKey) {
-    const listener = this.#events[callerId][type]
+    const listener = this.#events[callerId][type];
     if (!listener) {
-      return
+      return;
     }
-      this.#bus.removeEventListener(type, listener)
-    delete this.#events[callerId][type]
+    this.#bus.removeEventListener(type, listener);
+    delete this.#events[callerId][type];
   }
 
   trigger(type: EventKey, detail?: unknown) {
-    this.#bus.dispatchEvent(new CustomEvent(type, { detail })) 
+    this.#bus.dispatchEvent(new CustomEvent(type, { detail }));
   }
 }
