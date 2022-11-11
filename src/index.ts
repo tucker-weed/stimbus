410e4919c901adaa2f03ca7fbbd5db38bd7b4aa3
Tucker Weed <tuckerjweed@gmail.com>
2022-11-11 11:59:14 -0500

    [fix] another patch to constructor Context

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,20 @@ import EventBus from "./util/bus";
 export default function Stimbus<T, K extends keyof T, TBase extends Constructor>(Base: TBase) {
   return class extends Base {
     readonly #eventBus = new EventBus<T, K>();
-    declare context: Context;
+    declare identifier: string;
 
     constructor(...args: any[]) {
-      super(args);
-      this.context = args as unknown as Context;
+      const context = args as unknown as Context;
+      super(context);
+      this.identifier = context.scope.identifier;
     }
 
     on(type: K, listener: Listener) {
-      this.#eventBus.addEvent(this.context.identifier, type, listener);
+      this.#eventBus.addEvent(this.identifier, type, listener);
     }
 
     off(type: K) {
-      this.#eventBus.removeEvent(this.context.identifier, type);
+      this.#eventBus.removeEvent(this.identifier, type);
     }
 
     trigger(type: K, detail?: unknown) {
