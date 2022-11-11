4cbe4c9415e26cf19404be8c86f324389f57fc53
Tucker Weed <tuckerjweed@gmail.com>
2022-11-11 11:34:01 -0500

    [feat] set context in mixin constructor

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
-import { Context } from "@hotwired/stimulus";
+import { Context, Controller } from "@hotwired/stimulus";
 import { Listener, Constructor } from "./types/env";
 import EventBus from "./util/bus";
 
 export default function Stimbus<T, K extends keyof T, TBase extends Constructor>(Base: TBase) {
   return class extends Base {
     readonly #eventBus = new EventBus<T, K>();
-    declare context: Context
+    declare context: Context;
+
+    constructor(...args: any[]) {
+      super(...args);
+      this.context = args[0] as Context;
+    }
 
     on(type: K, listener: Listener) {
       this.#eventBus.addEvent(this.context.identifier, type, listener);
@@ -18,5 +23,5 @@ export default function Stimbus<T, K extends keyof T, TBase extends Constructor>
     trigger(type: K, detail?: unknown) {
       return this.#eventBus.trigger(type, detail);
     }
-  }
+  };
 }
